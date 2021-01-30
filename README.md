1f59dea136fd0bb0f48067ddacf2cc79dc283118
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2021-01-30 19:54:48 -0300

    Adicionando next-app e db.json

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Mãe de Planta Quiz
 
+![Home page do quiz](HomePage.png)
+
 Desenvolvido durante a segunda imersão react da Alura, o quiz é uma forma divertida de testar seus conhecimentos e aprender um pouco mais
 sobre plantinhas.
 
---
+++ mae-de-planta-quiz/pages/index.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import db from '../db.json';
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg}).
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`
+
+export default function Home() {
+  return(
+    <BackgroundImage>
+      My paaaaaaaaaaaaaaaaaaaaage
+    </BackgroundImage>
+    )
+}
